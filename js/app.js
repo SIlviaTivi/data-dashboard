e89dfb11c34d236f1cf1b879a8881650d1a80907
SIlviaTivi <skfl.erg@hotmail.com>
2017-11-28 10:58:53 -0500

    prueba1

--- js/app.js
+++ js/app.js
@@ -2,5 +2,18 @@
  * Funcionalidad de tu producto
  */
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+
+// suma total de estudiantes presentes por sede y generacion
+// por sede:
+var dataLIM20162 = data.LIM['2016-2'].students.length + 1;
+console.log(dataLIM20162);
+var dataLIM20171 = data.LIM['2017-1'].students.length + 1;
+console.log(dataLIM20171);
+var dataLIM20172 = data.LIM['2017-2'].students.length + 1;
+console.log(dataLIM20172);
+
+var numberEstudentsSeat = [dataLIM20162, dataLIM20171, dataLIM20172];
+
+for (var i = 0; i < numberEstudentsSeat.length; i++) {
+console.log(numberEstudentsSeat[i]);
+};
